2d581a75d3826b96c445737ca196fe2198aff36e
Lucas Arantes <lucas.ruby.dev@gmail.com>
2026-05-08 22:36:52 -0400

    feat(auth): authBroker for GitHub session and PAT settings

---
+++ src/auth/authBroker.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+import { readConfig } from '../config';
+
+export class AuthBroker {
+  async getGithubToken(silent: boolean): Promise<string | undefined> {
+    try {
+      const session = await vscode.authentication.getSession('github', ['repo'], { createIfNone: !silent, silent });
+      return session?.accessToken;
+    } catch {
+      return undefined;
+    }
+  }
+
+  getGitlabToken(): string | undefined {
+    const t = readConfig().gitlabToken;
+    return t || undefined;
+  }
+
+  getBitbucketToken(): string | undefined {
+    const t = readConfig().bitbucketToken;
+    return t || undefined;
+  }
+}
